bbc071eea43162185a317191faf1683a57d9c350
Bo-Chun Louis Chen <louistw@uab.edu>
2020-04-09 00:06:14 -0500

    First draft task manager

---
+++ task_manager.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+import sys
+import json
+from rc_rmq import RCRMQ
+from datetime import datetime
+
+task = 'task_manager'
+
+record = {
+    'uid': -1,
+    'gid': -1,
+    'email': '',
+    'last_update': datetime.now(),
+    'request': {
+        'uid_resolve': None
+    },
+    'create': {
+        'join_list': None,
+        'create_account': None
+    },
+    'verify': {
+        'git_commit': None,
+        'dir_verify': None
+    }
+}
+
+# Currently tracking users
+tracking = {}
+
+# Instantiate rabbitmq object
+rc_rmq = RCRMQ({'exchange': 'RegUsr', 'exchange_type': 'topic'})
+
+def task_manager(ch, method, properties, body):
+    msg = json.loads(body)
+    username = method.routing_key.split('.')[1]
+    task_name = msg['task']
+    success = msg['success']
+
+    if username not in tracking:
+        current = tracking[username] = record.copy()
+        current['uid'] = msg.get('uid', -1)
+        current['gid'] = msg.get('gid', -1)
+        current['email'] = msg.get('email', '')
+
+    try:
+    # Check each level
+    # task timeout
+    # failure agent(?
+        if task_name in current['request']:
+            current['request'][task_name] = success
+            routing_key = 'create.' + username
+            done = success
+
+        elif task_name in current['create']:
+            current['create'][task_name] = success
+            routing_key = 'verify.' + username
+            done = True
+            for status in current['create'].values():
+                if status is not True:
+                    done = False
+
+        elif task_name in current['verify']:
+            current['verify'][task_name] = success
+            routing_key = 'notify.' + username
+            done = True
+            for status in current['verify'].values():
+                if status is not True:
+                    done = False
+
+    except:
+        e = sys.exc_info()[0]
+        print("[{}]: Error: {}".format(task, e))
+
+    if done:
+        # send trigger message
+        rc_rmq.publish_msg({
+            'routing_key': routing_key
+            'msg': {
+                'username': username,
+                'email': current['email'],
+                'uid': current['uid'],
+                'gid': current['gid']
+            }
+        })
+
+print("Start listening to queue: {}".format(task))
+rc_rmq.start_consume({
+    'queue': task,
+    'routing_key': "confirm.*",
+    'cb': task_manager
+})
+
+print("Disconnected")
+rc_rmq.disconnect()
