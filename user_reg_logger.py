07e06bc818f6c527c2fe89635c3a2a4cca2391c2
Eesaan Atluri <atlurie@uab.edu>
2020-04-14 01:49:21 +0000

    Change the table name to represent account req

--- user_reg_logger.py
+++ user_reg_logger.py
@@ -20,14 +20,14 @@ logger = rc_util.get_logger()
 
 # Open registry table in DB
 db = dataset.connect('sqlite:///reg_logger.db')
-table = db['registry']
+account_req_table = db['registry']
 
 # Define registration logger callback
 def log_registration(ch, method, properties, body):
 
     account_req = json.loads(body)
-    table.insert(account_req)
     account_req['req_time'] = datetime.now(),
+    account_req_table.insert(account_req)
     logger.info("logged account request for %s", account_req['username'])
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
