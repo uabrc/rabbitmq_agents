660def700c04fcfb5dad3c934dcc0715064a306d
Bo-Chun Louis Chen <louistw@uab.edu>
2020-04-20 22:47:26 -0500

    Remove git diff step

--- git_commit.py
+++ git_commit.py
@@ -73,8 +73,6 @@ def git_commit(ch, method, properties, body):
             ldapsearch('-LLL', '-x', '-h', 'ldapserver', '-b', "'ou=Group,dc=cm,dc=cluster'", f"cn={username}", _out=ldif_g)
         logger.info('user ldif files generated.')
 
-        logger.debug('git diff')
-        git.diff()
         logger.debug('git add %s', user_ldif)
         git.add(user_ldif)
         logger.debug('git add %s', group_ldif)
