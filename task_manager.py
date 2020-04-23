9f499aece5f6e7c6e55bbe37f5030e1b0dac3dae
Bo-Chun Louis Chen <louistw@uab.edu>
2020-04-23 17:58:18 -0500

    Utilize rc_util

--- task_manager.py
+++ task_manager.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import sys
 import json
+import rc_util
 from rc_rmq import RCRMQ
 from datetime import datetime
 
 task = 'task_manager'
+
+args = rc_util.get_args()
+logger = rc_util.get_logger(args)
 
 record = {
     'uid': -1,
@@ -46,6 +50,8 @@ def task_manager(ch, method, properties, body):
         current['uid'] = msg.get('uid', -1)
         current['gid'] = msg.get('gid', -1)
         current['email'] = msg.get('email', '')
+
+        logger.debug(f'Tracking user {username}')
     else:
         current = tracking[username]
 
@@ -61,6 +67,8 @@ def task_manager(ch, method, properties, body):
             routing_key = 'create.' + username
             done = success
 
+            logger.debug(f'Request level task(s) done?{done}')
+
         elif task_name in current['create']:
             current['create'][task_name] = success
             routing_key = 'verify.' + username
@@ -68,6 +76,8 @@ def task_manager(ch, method, properties, body):
             for status in current['create'].values():
                 if status is not True:
                     done = False
+
+            logger.debug(f'Create level task(s) done?{done}')
 
         elif task_name in current['verify']:
             current['verify'][task_name] = success
@@ -77,20 +87,25 @@ def task_manager(ch, method, properties, body):
                 if status is not True:
                     done = False
 
+            logger.debug(f'Verify level task(s) done?{done}')
+
         elif task_name in current['notify']:
             current['verify'][task_name] = success
             routing_key = 'complete.' + username
             done = success
 
-    except:
-        e = sys.exc_info()[0]
-        print("[{}]: Error: {}".format(task, e))
+            logger.debug(f'Notify level task(s) done?{done}')
+
+    except Exception as exception:
+        logger.error('', exc_info=True)
 
     if done:
         # acknowledge all message from last level
         for tag in current['delivery_tags']:
             ch.basic_ack(tag)
         current['delivery_tags'] = []
+
+        logger.debug('Previous level messages acknowledged')
 
         # send trigger message
         rc_rmq.publish_msg({
@@ -103,13 +118,15 @@ def task_manager(ch, method, properties, body):
             }
         })
 
+        logger.debug(f"Trigger message '{routing_key}' sent")
 
-print("Start listening to queue: {}".format(task))
+
+logger.info(f'Start listening to queue: {task}')
 rc_rmq.start_consume({
     'queue': task,
     'routing_key': "confirm.*",
     'cb': task_manager
 })
 
-print("Disconnected")
+logger.info('Disconnected')
 rc_rmq.disconnect()
