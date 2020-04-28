1b5c98e97bd99a85966c0a2678ee08fa4d41ed71
Bo-Chun Louis Chen <louistw@uab.edu>
2020-04-28 17:25:57 -0500

    Utilize argparse library

--- create_account.py
+++ create_account.py
@@ -1,25 +1,22 @@
 #!/usr/bin/env python3
-import sys
 import rc_util
+import argparse
 
-if len(sys.argv) < 2:
-    print("Usage: {} USERNAME [EMAIL] [FULL_NAME] [REASON]".format(sys.argv[0]), file=sys.stderr)
-    exit(1)
+parser = argparse.ArgumentParser()
+parser.add_argument('username', help='username that will be created')
+parser.add_argument('email', nargs='?', default='', help="User's email")
+parser.add_argument('full_name', nargs='?', default='', help="User's full name")
+parser.add_argument('reason', nargs='?', default='', help='Reason of requesting')
+parser.add_argument('--domain', default='localhost', help='domain of email')
+args = parser.parse_args()
 
-domain = 'uab.edu'
-user_name = sys.argv[1]
-email = sys.argv[2] if len(sys.argv) >= 3 else ''
-full_name = sys.argv[3] if len(sys.argv) >= 4 else ''
-reason    = sys.argv[4] if len(sys.argv) >= 5 else ''
+if args.email == '':
+    args.email = args.username
+    if '@' not in args.email:
+        args.email = args.username + '@' + args.domain
 
-if email == '':
-    if '@' in user_name:
-        email = user_name
-    else:
-        email = user_name + '@' + domain
-
-rc_util.add_account(user_name, email=email, full=full_name, reason=reason)
-print("Account requested for user: {}".format(user_name))
+rc_util.add_account(args.username, email=args.email, full=args.full_name, reason=args.reason)
+print(f'Account for {args.username} requested.')
 
 print("Waiting for confirmation...")
-rc_util.consume(user_name)
+rc_util.consume(args.username)
