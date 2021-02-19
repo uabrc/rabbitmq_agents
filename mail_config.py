6ecabd9e0274d1dfe7ab11273309390842bb0166
Eesaan Atluri <atlurie@uab.edu>
2021-02-19 14:25:26 -0600

    Move mail cfg vars to rabbit config, keep only mail template.

--- mail_config.py
+++ mail_config.py
@@ -1,14 +1,8 @@
-# Some variable for email
-Server = 'localhost'
-Admin_email = 'root@localhost'
-Sender = 'ROOT@LOCALHOST'
-Sender_alias = 'Services'
-Subject = 'New User Account'
-Info_url = 'https://www.google.com'
+import rabbit_config as rcfg
 
-Head = f"""From: {Sender_alias} <{Sender}>
+Head = f"""From: {rcfg.Sender_alias} <{rcfg.Sender}>
 To: <{{{{ to }}}}>
-Subject: {Subject}
+Subject: {rcfg.Subject}
 """
 
 Body = f"""
@@ -20,16 +14,16 @@ User ID:  {{{{ username }}}}
 ============================
 
 If you have any questions, please visit:
-{Info_url}
+{rcfg.Info_url}
 
-or email at {Admin_email}
+or email at {rcfg.Admin_email}
 
 Cheers,
 """
 
 Whole_mail = Head + Body
 
-UserReportHead = f"""From: {Sender_alias} <{Sender}>
-To: <{Admin_email}>
+UserReportHead = f"""From: {rcfg.Sender_alias} <{rcfg.Sender}>
+To: <{rcfg.Admin_email}>
 Subject: RC Account Creation Report: {{{{ fullname }}}}, {{{{ username }}}} """
 
