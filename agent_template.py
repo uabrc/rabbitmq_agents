1905ff324adec6e2fe9137a162df3f4a8d12a329
Bo-Chun Louis Chen <louistw@uab.edu>
2021-05-03 20:37:03 -0500

    Reformatting whole project

--- agent_template.py
+++ agent_template.py
@@ -3,10 +3,10 @@ import sys
 import json
 from rc_rmq import RCRMQ
 
-task = 'task_name'
+task = "task_name"
 
 # Instantiate rabbitmq object
-rc_rmq = RCRMQ({'exchange': 'RegUsr', 'exchange_type': 'topic'})
+rc_rmq = RCRMQ({"exchange": "RegUsr", "exchange_type": "topic"})
 
 # Define your callback function
 def on_message(ch, method, properties, body):
@@ -18,15 +18,17 @@ def on_message(ch, method, properties, body):
     msg = json.loads(body)
 
     # Do Something
-    print('[{}]: Callback called.'.format(task))
+    print("[{}]: Callback called.".format(task))
 
     # Acknowledge message
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 print("Start listening to queue: {}".format(task))
-rc_rmq.start_consume({
-    'queue': task,      # Define your Queue name
-    'routing_key': "#", # Define your routing key
-    'cb': on_message    # Pass in callback function you just define
-})
+rc_rmq.start_consume(
+    {
+        "queue": task,  # Define your Queue name
+        "routing_key": "#",  # Define your routing key
+        "cb": on_message,  # Pass in callback function you just define
+    }
+)
