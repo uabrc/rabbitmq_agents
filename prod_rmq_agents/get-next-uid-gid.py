d81faa730305978c89581da7eb45fe95b41e5ca9
Ravi Tripathi <ravi89@uab.edu>
2021-05-13 13:50:03 -0500

    In Bright 8.2 , parameter to add userid for a user is userid rather then the id that comes into picture with Bright version 9.2

--- prod_rmq_agents/get-next-uid-gid.py
+++ prod_rmq_agents/get-next-uid-gid.py
@@ -36,7 +36,7 @@ def create_account(msg):
 
     # Bright command to create user
     cmd = "/cm/local/apps/cmd/bin/cmsh -c "
-    cmd += f'"user; add {username}; set id {uid}; set email {email}; set commonname \\"{fullname}\\"; '
+    cmd += f'"user; add {username}; set userid {uid}; set email {email}; set commonname \\"{fullname}\\"; '
     cmd += 'commit;"'
 
     if not args.dry_run:
