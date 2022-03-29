2826a1eec2f75b8c019a0975204a0a87c97f06af
Bo-Chun Louis Chen <louistw@uab.edu>
2022-03-28 20:42:11 -0500

    Add migration flag
    During the first phase, e.g. not all of users are in the user_state DB,
      using alternative method, getnet passwd, to see if user is an existing
      user.

--- prod_rmq_agents/examples/user_auth.py
+++ prod_rmq_agents/examples/user_auth.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 import os
 import sys
+import subprocess
 
 sys.path.insert(1, os.path.join(sys.path[0], "../.."))
 import rc_util
 
 
+# During migration of this new script for ood
+# e.g. not all of users are in the db
+migration = True
+# migration = False  # uncomment after migration's done
 remote_user = sys.argv[1]
 
 result = rc_util.check_user(remote_user)
@@ -13,4 +18,9 @@ result = rc_util.check_user(remote_user)
 if result == "ok":
     print(remote_user)
 else:
+    if migration:
+        rc = subprocess.run(["getent", "passwd", remote_user]).returncode
+        if rc == 0:
+            print(remote_user)
+            sys.exit()
     print()
