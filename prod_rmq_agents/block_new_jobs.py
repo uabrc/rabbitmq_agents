e092a4194f1df6cff3fb448b476c097f1ead82ce
atlurie <atlurie@uab.edu>
2022-04-07 11:37:59 -0500

    RMQ agent to block new jobs from getting to run state based on lock/unlock action passed in the message.

--- prod_rmq_agents/block_new_jobs.py
+++ prod_rmq_agents/block_new_jobs.py
@@ -6,54 +6,61 @@ from pathlib import Path
 from rc_rmq import RCRMQ
 import rabbit_config as rcfg
 
-task = "block_new_jobs"
+task = "new_jobs"
 
 args = rc_util.get_args()
 logger = rc_util.get_logger(args)
 
 # Instantiate rabbitmq object
-rc_rmq = RCRMQ({"exchange": "", "exchange_type": "topic"})
+rc_rmq = RCRMQ({"exchange": "RegUsr", "exchange_type": "topic"})
 
 
-def block_new_jobs(ch, method, properties, body):
+def new_jobs(ch, method, properties, body):
     msg = json.loads(body)
     username = msg["username"]
-    msg["task"] = task
+    action = msg["action"]
     msg["success"] = False
 
+    corr_id = properties.correlation_id
     reply_to = properties.reply_to
 
     try:
-        block_ssh_cmd = f"/cm/shared/apps/slurm/19.05.5/bin/sacctmgr --immediate update user {username} set maxjobs=0"
-        block_ssh = popen(block_ssh_cmd).read().rstrip()
+        block_new_jobs_cmd = f"/cm/shared/apps/slurm/19.05.5/bin/sacctmgr --immediate update user {username} set maxjobs=0"
+        unblock_new_jobs_cmd = f"/cm/shared/apps/slurm/19.05.5/bin/sacctmgr --immediate update user {username} set maxjobs=-1"
 
-    msg["success"] = True
+        if action == 'lock':
+            block_new_jobs = popen(block_new_jobs_cmd).read().rstrip()
+        elif action == 'unlock':
+            unblock_new_jobs = popen(unblock_new_jobs_cmd).read().rstrip()
+
+        msg["success"] = True
 
     except Exception:
         msg["success"] = False
         msg["errmsg"] = "Exception raised while setting maxjobs that can enter run state, check the logs for stack trace"
         logger.error("", exc_info=True)
 
-    # send confirm message
-    rc_rmq.publish_msg(
-        {"routing_key": reply_to, 
-         "props": pika.BasicProperties(
-                            reply_to = callback_queue,
-                            ),
-         "msg": msg}
-    )
+    # send response to callback queue with it's correlation ID
+    if reply_to:
+        rc_rmq.publish_msg(
+            {"routing_key": reply_to, 
+             "props": pika.BasicProperties(
+                      correlation_id=corr_id,
+                      ),
+             "msg": msg}
+        )
 
-    logger.debug(f"User {username} confirmation sent for {task}")
+    logger.debug(f"User {username} confirmation sent for {action}ing {task}")
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
 logger.info(f"Start listening to queue: {task}")
+rc_rmq.bind_queue(queue=task, routing_key='lock.*', durable=True)
+rc_rmq.bind_queue(queue=task, routing_key='unlock.*', durable=True)
+rc_rmq.bind_queue(queue=task, routing_key='newjobs.*', durable=True)
 rc_rmq.start_consume(
-    {"queue": task, "routing_key": "block.*", "cb": block_new_jobs}
-)
-rc_rmq.start_consume(
-    {"queue": task, "routing_key": "block.newjobs.*", "cb": block_new_jobs}
+    {"queue": task, "cb": new_jobs}
 )
 
 logger.info("Disconnected")
